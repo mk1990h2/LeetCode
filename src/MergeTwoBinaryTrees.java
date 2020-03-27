246b9bf7837770f5c2af1217624e6c67109a9928
Mika <mikass111@yahoo.co.jp>
2020-03-26 20:52:33 -0700

    merge two binary trees

---
+++ src/MergeTwoBinaryTrees.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/merge-two-binary-trees/
+public class MergeTwoBinaryTrees {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+
+        return root;
+    }
+
+//    public TreeNode mergeTreesHelper(TreeNode t1, TreeNode t2) {
+//        TreeNode newNode;
+//
+//        if (t1 == null && t2 == null) return null;
+//        if (t1 == null) {
+//            newNode = new TreeNode(t2.val);
+//            newNode.left = mergeTreesHelper(null, t2.left);
+//            newNode.right = mergeTreesHelper(null, t2.right);
+//        } else if (t2 == null) {
+//            newNode = new TreeNode(t1.val);
+//            newNode.left = mergeTreesHelper(t1.left, null);
+//            newNode.right = mergeTreesHelper(t1.right, null);
+//        } else {
+//            newNode = new TreeNode(t1.val + t2.val);
+//            newNode.left = mergeTreesHelper(t1.left, t2.left);
+//            newNode.right = mergeTreesHelper(t1.right, t2.right);
+//        }
+//
+//        return newNode;
+//    }
+}
