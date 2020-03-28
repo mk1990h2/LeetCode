19f9eb12a14b8fbb2db703a3dbcf71563a7e3c1c
Mika <mikass111@yahoo.co.jp>
2020-03-27 18:54:51 -0700

    single number

---
+++ src/SingleNumber.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans =0;
+
+        for (int num : nums) {
+            ans ^= num;
+        }
+
+        return ans;
+    }
+
+//    public int singleNumber(int[] nums) {
+//        HashMap<Integer, Integer> hm = new HashMap<>();
+//        for (int num : nums) {
+//            if (hm.get(num) != null) hm.remove(num);
+//            else hm.put(num, 1);
+//        }
+//        Set<Integer> keys = hm.keySet();
+//        Object[] keysArray = keys.toArray();
+//
+//        return (int) keysArray[0];
+//    }
+}
