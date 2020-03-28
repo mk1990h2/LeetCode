3c1046d6df0ccb9ee50152d5da47aa9f1314225d
Mika <mikass111@yahoo.co.jp>
2020-03-27 18:26:26 -0700

    maximum depth of binary tree

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+//    private int maxDepthHelper(TreeNode node, int depth) {
+//        if (node == null) {
+//            return depth;
+//        }
+//        depth++;
+//        return Math.max(maxDepthHelper(node.left, depth), maxDepthHelper(node.right, depth));
+//    }
+}
