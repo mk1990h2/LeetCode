659829134a8843ecde31783eeeaf6992487bd3b6
Mika <mikass111@yahoo.co.jp>
2020-03-27 21:29:47 -0700

    invert binary tree

---
+++ src/InvertBinaryTree.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/invert-binary-tree/
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode tmp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(tmp);
+
+        return root;
+    }
+}
