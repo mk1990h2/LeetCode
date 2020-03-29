bf900788eb38b26c9b2a404c98abeeab55010555
Mika <mikass111@yahoo.co.jp>
2020-03-28 17:43:08 -0700

    diameter of binary tree

---
+++ src/easy/DiameterOfBinaryTree.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// https://leetcode.com/problems/diameter-of-binary-tree/
+public class DiameterOfBinaryTree {
+    // TODO: I couldn't solve by my self
+    int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameterOfBinaryTreeHelper(root);
+        return max;
+    }
+
+    private int diameterOfBinaryTreeHelper(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = diameterOfBinaryTreeHelper(root.left);
+        int right = diameterOfBinaryTreeHelper(root.right);
+        max = Math.max(max, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+}
