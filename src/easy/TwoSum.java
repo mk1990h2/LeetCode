1efac444007256a469ade4dbc617d4251652ba6e
Mika <mikass111@yahoo.co.jp>
2020-03-28 23:01:39 -0700

    two sum

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/two-sum/
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;
+    }
+
+    // slow
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            int remainder = target - nums[i];
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[j] == remainder) {
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//
+//        return new int[0];
+//    }
+}
