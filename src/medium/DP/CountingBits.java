fa6c21363ffef85e9f988b553a903172f6388b76
Mika <mikass111@yahoo.co.jp>
2020-04-06 05:14:21 -0700

    counting bits

--- src/medium/DP/CountingBits.java
+++ src/medium/DP/CountingBits.java
@@ -1,6 +1,28 @@
 package medium.DP;
 
 // https://leetcode.com/problems/counting-bits/
+
+/**
+ * Explanation:
+ * First of all, I examine several numbers to find out the pattern.
+ * [0]: 000 -> 0
+ * [1]: 001 -> 1
+ * [2]: 010 -> 1
+ * [3]: 011 -> 2
+ * [4]: 100 -> 1
+ * [5]: 101 -> 2
+ * [6]: 110 -> 2
+ * [7]: 111 -> 3
+ * Now I can see the pattern. It is a kind of loop from 1 to 2 power of x.
+ * As you can see, 2 power of x is always 1. How about other numbers?
+ * For example, 5 can be represented like this: [1] + 1.
+ * 6 can be represented like this: [2] + 1.
+ * Needless to say, 7 can be represented like this: [3] + 1.
+ * So, regarding 2 power of x as a starting point, I can refer to corresponding index.
+ *
+ * Time Complexity: O(n)
+ * Space: n
+ */
 public class CountingBits {
     public int[] countBits(int num) {
         if (num == 0) return new int[]{0};
